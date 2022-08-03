00104f87c90ea15864a685525581c1a2bab435e0
rahulmr96 <rahulmranga@gmail.com>
2022-08-03 14:14:41 -0500

    Checkin Checkout -works

--- cico.py
+++ cico.py
@@ -6,6 +6,6 @@ from dbquery import projects_existing, create_project
 cico=Blueprint('cico',__name__,static_folder='build', static_url_path='/')
 cors = CORS(cico, resources={r"/api/*": {"origins": "*"}})
 
-@cico.app('/api/projects/<id>/gethwdata/')
+@cico.route('/api/projects/<id>/gethwdata/')
 def dn():
     return json.dumps({'hwSet1':'20'})
