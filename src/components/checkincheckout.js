165c176c1f3f4303af0f09dca777c14b8ec3dabb
rahulmr96 <rahulmranga@gmail.com>
2022-08-03 13:56:05 -0500

    akdnsa

--- src/components/checkincheckout.js
+++ src/components/checkincheckout.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect,useState} from 'react'
 import { useParams,useNavigate } from 'react-router-dom';
 import '/node_modules/bootstrap/dist/css/bootstrap.min.css'
 
