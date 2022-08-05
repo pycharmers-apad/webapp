0449967ef3358a27037a08775e5676654ca25cd6
rahulmr96 <rahulmranga@gmail.com>
2022-08-05 00:36:41 -0500

    Signup page

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -25,7 +25,7 @@ const onSubmit = async (event) => { //Submit the event, and send the username an
 
 }
 useEffect(()=>{
-  if(output!=='Credentials Authenticated'){
+  if(output=='Credentials Authenticated'){
   console.log(output)
   navigate('/projects')}
 },[output])
